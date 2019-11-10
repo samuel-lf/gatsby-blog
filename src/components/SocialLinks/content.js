230cc8661104f30ac927b7c5f676db28df2ead7b
Samuel Leonel <samueleonelfreitas@hotmail.com>
2019-11-10 14:15:35 -0300

    Criação de menu de social medias

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+    {
+        label: 'Github',
+        url: 'https://github.com/samuel-lf'
+    },
+    {
+        label: 'Instagram',
+        url: 'https://www.instagram.com/samuel_lf'
+    },
+    {
+        label: 'Twitter',
+        url: 'https://twitter.com/Samuel__Leonel'
+    }
+]
+
+export default links;
