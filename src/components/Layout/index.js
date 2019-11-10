e1d1ffb29b1121127974ac99390d09dbfca1d721
Samuel Leonel <samueleonelfreitas@hotmail.com>
2019-11-10 13:52:27 -0300

    Criação da sidebar e pequenos ajustes

---
+++ src/components/Layout/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import PropTypes from "prop-types"
+import GlobalStyle from "../../styles/global";
+import * as S from './styled'
+import Sidebar from "../Sidebar";
+
+const Layout = ({ children }) => {
+
+  return (
+    <S.LayoutWrapper>
+      <GlobalStyle />
+      <Sidebar/>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
